521c0737cc92fcde6ed0597118fe7c2a9f8044ca
bb434 <bb434@njit.edu>
2020-09-18 02:29:34 -0400

    Java Homework 1

---
+++ JavaFiles/src/HW1.java
@@ -0,0 +1,17 @@
+
+public class HW1 {
+	public static void main(String[] args) {
+		String studentName = "Barr Beneli";
+		
+		System.out.println("Student's name is " + studentName);
+		
+		int studentAge = 19;
+		
+		System.out.println("Student's age is " + studentAge);
+		
+		float studentGPA = 3.9f;
+		
+		System.out.println("Student's GPA is " + studentGPA);
+	}
+
+}
